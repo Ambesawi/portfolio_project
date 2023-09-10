2df3071a725bcb42a261fceed5b9c432ca037a4b
NebihaDilsebo <shegolenur@hotmail.com>
2023-09-10 14:39:15 -0700

    portfolio

---
+++ models/user.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" holds class User"""
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        UserID = Column(INT(20), nullable=False)
+        FirstName = Column(CHAR(20), nullable=False)
+        LastName = Column(CHAR(20), nullable=False)
+        Email = Column(VARCHAR(45), nullable=False)
+        Password = Column(VARCHAR(45), nullable=False)
+	Address = Column(VARCHAR(45), nullable=False)
+	PhoneNumber = Column(INT(45), nullable=False)
+        orders = relationship("Order", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
